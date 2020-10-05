6716f8fe6943e956809006d519e14e0f3f657765
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-05 20:57:15 +0700

    handler function

--- gwp/chapter3/main.go
+++ gwp/chapter3/main.go
@@ -5,39 +5,21 @@ import (
 	"net/http"
 )
 
-// MyHandler ...
-type MyHandler struct{}
-
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home!")
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
 }
 
-// HelloHandler ...
-type HelloHandler struct{}
-
-func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
-}
-
-// WorldHandler ...
-type WorldHandler struct{}
-
-func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func world(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "World!")
 }
 
 func main() {
-	handler := MyHandler{}
-	hello := HelloHandler{}
-	world := WorldHandler{}
 
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-
-	http.Handle("/", &handler)
-	http.Handle("/hello", &hello)
-	http.Handle("/world", &world)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/world", world)
 
 	server.ListenAndServeTLS("cert.pem", "key.pem")
 }
