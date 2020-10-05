9d2b4d413c4b668547669868f0e47bae6bd99f8d
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-05 20:35:23 +0700

    multiple handlers

--- gwp/chapter3/main.go
+++ gwp/chapter3/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+// MyHandler ...
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home!")
+}
+
+// HelloHandler ...
+type HelloHandler struct{}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+// WorldHandler ...
+type WorldHandler struct{}
+
+func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
+}
+
 func main() {
+	handler := MyHandler{}
+	hello := HelloHandler{}
+	world := WorldHandler{}
+
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: nil,
+		Addr: "127.0.0.1:8080",
 	}
+
+	http.Handle("/", &handler)
+	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
+
 	server.ListenAndServeTLS("cert.pem", "key.pem")
 }
