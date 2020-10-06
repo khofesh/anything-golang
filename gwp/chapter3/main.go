e486220f741d7504b91f5c90b4f74a2373c759a7
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-06 15:43:35 +0700

    chaining handlers

--- gwp/chapter3/main.go
+++ gwp/chapter3/main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"reflect"
-	"runtime"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+// HelloHandler ...
+type HelloHandler struct{}
+
+func (h HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
-func log(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-		fmt.Println("Handler function called - " + name)
-		h(w, r)
-	}
+func log(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Handler called - %T\n", h)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func protect(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
@@ -24,7 +30,8 @@ func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/hello", log(hello))
+	hello := HelloHandler{}
+	http.Handle("/hello", protect(log(hello)))
 
 	server.ListenAndServeTLS("cert.pem", "key.pem")
 }
