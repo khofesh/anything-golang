215734b46b89d2c81939a21cffdf4d28eac15fd7
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-10 14:47:43 +0700

    multiple handler using httprouter

--- gwp/chapter3/main.go
+++ gwp/chapter3/main.go
@@ -3,35 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-// HelloHandler ...
-type HelloHandler struct{}
-
-func (h HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
-}
-
-func log(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Handler called - %T\n", h)
-		h.ServeHTTP(w, r)
-	})
-}
-
-func protect(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		h.ServeHTTP(w, r)
-	})
+func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", p.ByName("name"))
 }
 
 func main() {
 
+	mux := httprouter.New()
+	mux.GET("/hello/:name", hello)
+
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
-	hello := HelloHandler{}
-	http.Handle("/hello", protect(log(hello)))
 
 	server.ListenAndServeTLS("cert.pem", "key.pem")
 }
