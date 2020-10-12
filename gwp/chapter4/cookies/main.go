5d9cc2cbc33b1fb1110de423cb57a91df29eca0c
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-12 09:54:53 +0700

    temporary message

--- gwp/chapter4/cookies/main.go
+++ gwp/chapter4/cookies/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func setCookie(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +32,33 @@ func getCookie(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, cs)
 }
 
+func setMessage(w http.ResponseWriter, r *http.Request) {
+	msg := []byte("Hello World!")
+	c := http.Cookie{
+		Name:  "flash",
+		Value: base64.URLEncoding.EncodeToString(msg),
+	}
+	http.SetCookie(w, &c)
+}
+
+func showMessage(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("flash")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			fmt.Fprintln(w, "No message found")
+		}
+	} else {
+		rc := http.Cookie{
+			Name:    "flash",
+			MaxAge:  -1,
+			Expires: time.Unix(1, 0),
+		}
+		http.SetCookie(w, &rc)
+		val, _ := base64.URLEncoding.DecodeString(c.Value)
+		fmt.Fprintln(w, string(val))
+	}
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
@@ -37,5 +66,8 @@ func main() {
 
 	http.HandleFunc("/set_cookie", setCookie)
 	http.HandleFunc("/get_cookie", getCookie)
+
+	http.HandleFunc("/set_message", setMessage)
+	http.HandleFunc("/show_message", showMessage)
 	server.ListenAndServe()
 }
