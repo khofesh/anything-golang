cc6fb86186dcc19860cc07247637bd5131a8e0ca
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-17 13:51:15 +0700

    iterator

---
+++ gwp/chapter5/iterator/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
+	t.Execute(w, daysOfWeek)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
