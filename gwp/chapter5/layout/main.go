1a299e9395c121c49b103fcbaf83f46b12aab970
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-17 21:08:09 +0700

    update

--- gwp/chapter5/layout/main.go
+++ gwp/chapter5/layout/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("layout.html")
+	rand.Seed(time.Now().Unix())
+	var t *template.Template
+	if rand.Intn(10) > 5 {
+		t, _ = template.ParseFiles("layout.html", "red_hello.html")
+	} else {
+		t, _ = template.ParseFiles("layout.html", "blue_hello.html")
+	}
 	t.ExecuteTemplate(w, "layout", "")
 }
 
