785ec67ee480559095bac1ec58e258691d9a18d2
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-17 20:44:48 +0700

    add comments

--- gwp/chapter5/xss/main.go
+++ gwp/chapter5/xss/main.go
@@ -8,7 +8,17 @@ import (
 func process(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("tmpl.html")
 	t.Execute(w, r.FormValue("comment"))
+	// not secure
+	// t.Execute(w, template.HTML(r.FormValue("comment")))
 }
+
+/**
+func process(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("X-XSS-Protection", "0")
+	t, _ := template.ParseFile("tmpl.html")
+	t.Execute(w, template.HTML(r.FormValue("comment")))
+}
+*/
 
 func form(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("form.html")
