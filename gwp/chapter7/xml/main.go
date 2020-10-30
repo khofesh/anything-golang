d29c006526e3e0ee8fa37188e557dda37a226ada
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-30 22:14:04 +0700

    update

--- gwp/chapter7/xml/main.go
+++ gwp/chapter7/xml/main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
 
 // Post ...
 type Post struct {
-	XMLName xml.Name `xml:"post"`
-	ID      string   `xml:"id,attr"`
-	Content string   `xml:"content"`
-	Author  Author   `xml:"author"`
-	XML     string   `xml:",innerxml"`
+	XMLName  xml.Name  `xml:"post"`
+	ID       string    `xml:"id,attr"`
+	Content  string    `xml:"content"`
+	Author   Author    `xml:"author"`
+	XML      string    `xml:",innerxml"`
+	Comments []Comment `xml:"comments>comment"`
+}
+
+// Comment ...
+type Comment struct {
+	ID      string `xml:"id,attr"`
+	Content string `xml:"content"`
+	Author  Author `xml:"author"`
 }
 
 // Author ...
@@ -29,6 +38,27 @@ func main() {
 		return
 	}
 	defer xmlFile.Close()
+
+	decoder := xml.NewDecoder(xmlFile)
+	for {
+		t, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println("Error decoding XML into tokens:", err)
+			return
+		}
+
+		switch se := t.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "comment" {
+				var comment Comment
+				decoder.DecodeElement(&comment, &se)
+			}
+		}
+	}
 
 	xmlData, err := ioutil.ReadAll(xmlFile)
 	if err != nil {
