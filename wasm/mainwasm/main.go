846324f3dad6bcbc06436c49b51cd756d561007f
fahmi ahmad <fahmiahmad@tutanota.com>
2020-12-29 23:57:58 +0700

    get 3 params

--- wasm/mainwasm/main.go
+++ wasm/mainwasm/main.go
@@ -3,18 +3,57 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+	"strconv"
 )
 
+func addOrSubtract(opType string, inputs []js.Value) (interface {}, error) {
+	value1 := js.Global().Get("document").Call("getElementById", inputs[0]).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", inputs[1]).Get("value").String()
+
+
+	int1, error := strconv.Atoi(value1)
+	if error != nil {
+		return nil, error
+	}
+
+	int2, error := strconv.Atoi(value2)
+	if error != nil {
+		return nil, error
+	}
+
+	if opType == "add" {
+		return int1 + int2, nil
+	} else if opType == "subtract" {
+		return int1 - int2, nil
+	}
+
+	return nil, nil
+}
+
 func add(this js.Value, inputs []js.Value) interface {} {
-	js.Global().Set("output", js.ValueOf(inputs[0].Int() + inputs[1].Int()))
-	fmt.Println(js.ValueOf(inputs[0].Int() + inputs[1].Int()))
+	result, error := addOrSubtract("add", inputs)
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	// js.Global().Set("output", js.ValueOf(inputs[0].Int() + inputs[1].Int()))
+	js.Global().Get("document").Call("getElementById", inputs[2].String()).Set("value", result)
+
+	fmt.Println(result)
 
 	return nil
 }
 
 func subtract(this js.Value, inputs []js.Value) interface {} {
-	js.Global().Set("output", js.ValueOf(inputs[0].Int() - inputs[1].Int()))
-	fmt.Println(js.ValueOf(inputs[0].Int() - inputs[1].Int()))
+	result, error := addOrSubtract("subtract", inputs)
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	// js.Global().Set("output", js.ValueOf(inputs[0].Int() + inputs[1].Int()))
+	js.Global().Get("document").Call("getElementById", inputs[2].String()).Set("value", result)
+
+	fmt.Println(result)
 
 	return nil
 }
