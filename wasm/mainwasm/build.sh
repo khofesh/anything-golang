bca7b0cf05f4161af96ac1312f5389820b0c2e6c
fahmi ahmad <fahmiahmad@tutanota.com>
2020-12-29 21:29:30 +0700

    get the first argument

--- wasm/mainwasm/build.sh
+++ wasm/mainwasm/build.sh
@@ -1,1 +1,7 @@
-GOOS=js GOARCH=wasm go build -o main.wasm
+if [ "$1" == "node" ]; then
+    GOOS=js GOARCH=wasm go run -exec="$(go env GOROOT)/misc/wasm/go_js_wasm_exec" .
+elif [ "$1" == "node-test" ]; then
+    GOOS=js GOARCH=wasm go test -exec="$(go env GOROOT)/misc/wasm/go_js_wasm_exec"
+else
+    GOOS=js GOARCH=wasm go build -o main.wasm
+fi
